2ba100f2efb6bbef5616687087b55692133fe92e
ishimwepati <wazacode@gmail.com>
2023-09-06 16:20:39 +0300

    Reseversed implemented

--- src/pages/rockets/rockets.js
+++ src/pages/rockets/rockets.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchSelectedRockets } from '../../redux/actions/rocketActions';
+import { fetchSelectedRockets, reserveRocket, cancelReservation } from '../../redux/actions/rocketActions';
 import './rockets.css';
 
 function Rockets() {
@@ -13,30 +13,52 @@ function Rockets() {
     dispatch(fetchSelectedRockets());
   }, [dispatch]);
 
+  const handleReserveClick = (rocketId) => {
+    dispatch(reserveRocket(rocketId));
+  };
+
+  const handleCancelClick = (rocketId) => {
+    dispatch(cancelReservation(rocketId));
+  };
+
   return (
-    <div className="rocket_body">
-      <h2>Checkout Available Rocket </h2>
-      <ul>
+    <div className="rocket-body">
+      <h2>Checkout Available Rockets</h2>
+      <ul className="rocket-list">
         {selectedRockets.map((rocket) => (
           <li key={rocket.id} className="rocket-item">
+            <div className="rocket-details">
+              <h3 className="rocket-name">{rocket.name}</h3>
+              <p className="rocket-description">
+                {rocket.reserved ? 'Reserved - ' : ''}
+                {rocket.description}
+              </p>
+              {rocket.reserved ? (
+                <button
+                  type="button" // Add type attribute
+                  className="cancel-reservation-button"
+                  onClick={() => handleCancelClick(rocket.id)}
+                >
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button
+                  type="button" // Add type attribute
+                  className="reserve-rocket-button"
+                  onClick={() => handleReserveClick(rocket.id)}
+                >
+                  Reserve Rocket
+                </button>
+              )}
+            </div>
             <img
               src={rocket.flickr_images[0]}
               alt={rocket.rocket_name}
               className="rocket-image"
             />
-            <div className="rocket-details">
-              <h3 className="rocket-name">
-                {' '}
-                {' '}
-                {' '}
-                {rocket.name}
-              </h3>
-              <p className="rocket-description">{rocket.description}</p>
-            </div>
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
--- src/redux/reducers/rocketReducer.js
+++ src/redux/reducers/rocketReducer.js
@@ -1,9 +1,9 @@
 // src/redux/reducers/rocketReducer.js
 
-import { FETCH_SELECTED_ROCKETS } from '../actions/types';
+import { FETCH_SELECTED_ROCKETS, RESERVE_ROCKET, CANCEL_RESERVATION } from '../actions/types';
 
 const initialState = {
-  selectedRockets: [],
+  selectedRockets: [], // Initial state for selected rockets data
 };
 
 const rocketReducer = (state = initialState, action) => {
@@ -11,7 +11,24 @@ const rocketReducer = (state = initialState, action) => {
     case FETCH_SELECTED_ROCKETS:
       return {
         ...state,
-        selectedRockets: action.payload,
+        selectedRockets: action.payload.map((rocket) => ({
+          ...rocket,
+          reserved: false, // Initialize the reserved property as false for all rockets
+        })),
+      };
+    case RESERVE_ROCKET:
+      return {
+        ...state,
+        selectedRockets: state.selectedRockets.map((rocket) => (
+          rocket.id === action.payload ? { ...rocket, reserved: true } : rocket
+        )),
+      };
+    case CANCEL_RESERVATION:
+      return {
+        ...state,
+        selectedRockets: state.selectedRockets.map((rocket) => (
+          rocket.id === action.payload ? { ...rocket, reserved: false } : rocket
+        )),
       };
     default:
       return state;
